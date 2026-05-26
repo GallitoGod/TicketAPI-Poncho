8e73a89bebf0adc73e07bf660af06a49d105e8dd
lautaro arce <lautaroarce345@gmail.com>
2026-05-26 20:28:11 -0300

    UPD 3

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Evento, SectorEntrada, Ticket
+from apps.core.serializer import EventoSerializer, SectorEntradaSerializer, TicketSerializer
 
-# Create your views here.
+class EventoViewSet(viewsets.ModelViewSet):
+    queryset = Evento.objects.all()
+    serializer_class = EventoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class SectorEntradaViewSet(viewsets.ModelViewSet):
+    queryset = SectorEntrada.objects.all()
+    serializer_class = SectorEntradaSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from .models import Usuario
 
-# Create your views here.
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['id', 'username', 'email', 'password', 'rol']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        usuario = Usuario.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password'],
+            rol=validated_data.get('rol', 'comprador') # Por defecto es comprador
+        )
+        return usuario
