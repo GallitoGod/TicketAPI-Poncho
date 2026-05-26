3e98b7d7c09d5943b584fc850dc018f8b57e424f
lautaro arce <lautaroarce345@gmail.com>
2026-05-26 20:46:05 -0300

    UPD 4

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,19 +1,7 @@
-from rest_framework import serializers
+from rest_framework import viewsets, permissions
 from .models import Usuario
+from apps.usuario.serializer import UsuarioSerializer
 
-class UsuarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Usuario
-        fields = ['id', 'username', 'email', 'password', 'rol']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-
-    def create(self, validated_data):
-        usuario = Usuario.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data.get('email', ''),
-            password=validated_data['password'],
-            rol=validated_data.get('rol', 'comprador') # Por defecto es comprador
-        )
-        return usuario
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
