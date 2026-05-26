507346a99d726ae2923935a1e479929581c0ee08
Pablo Gallo <gallopabloj@gmail.com>
2026-05-26 14:26:20 -0300

    actualizacion de requirements, modificacion de configuracion de DRF y simple JWT y creacion de diccionario REST_FRAMEWORK para autenticacion por JWT

---
+++ TicketAPI_Poncho/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TicketAPI_Poncho project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TicketAPI_Poncho.settings")
+
+application = get_asgi_application()
