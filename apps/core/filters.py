1032f9dd0f351e17b3e58ed8cb48affc6d73b06a
lautaro arce <lautaroarce345@gmail.com>
2026-05-27 22:35:20 -0300

    UPD 11 - Filters

---
+++ apps/core/filters.py
@@ -0,0 +1,25 @@
+import django_filters
+from .models import Evento, SectorEntrada, Ticket
+
+class EventoFilter(django_filters.FilterSet):
+
+    fecha_desde = django_filters.DateFilter(field_name="fecha", lookup_expr='gte')
+    fecha_hasta = django_filters.DateFilter(field_name="fecha", lookup_expr='lte')
+    
+    
+    lugar = django_filters.CharFilter(field_name="lugar", lookup_expr='icontains') # icontains hace la búsqueda sin importar mayúsculas
+
+    class Meta:
+        model = Evento
+        # Definimos qué campos exactos permitimos filtrar
+        fields = ['fecha', 'lugar'] 
+
+class SectorEntradaFilter(django_filters.FilterSet):
+    class Meta:
+        model = SectorEntrada
+        fields = ['evento']
+
+class TicketFilter(django_filters.FilterSet):
+    class Meta:
+        model = Ticket
+        fields = ['sector_entrada']
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,26 +1,40 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .models import Evento, SectorEntrada, Ticket
 from apps.core.serializer import EventoSerializer, SectorEntradaSerializer, TicketSerializer
 from apps.core.permissions import EsProductorOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import EventoFilter, SectorEntradaFilter, TicketFilter
 
 class EventoViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all()
     serializer_class = EventoSerializer
     permission_classes = [EsProductorOrReadOnly] 
     lookup_field = 'uuid'
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['nombre', 'artista_principal', 'descripcion']
+    ordering_fields = ['fecha', 'nombre']
+    filterset_class = EventoFilter
+
 
 class SectorEntradaViewSet(viewsets.ModelViewSet):
     queryset = SectorEntrada.objects.all()
     serializer_class = SectorEntradaSerializer
     permission_classes = [EsProductorOrReadOnly]  
     lookup_field = 'uuid'
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    ordering_fields = ['precio_base_ars']
+    filterset_class = SectorEntradaFilter
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
     permission_classes = [permissions.IsAuthenticated]     # Solo los usuarios con token válido pueden entrar acá
     lookup_field = 'uuid'
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    ordering_fields = ['fecha_transaccion', 'precio_final_ars']
+    filterset_class = TicketFilter
 
     # Esto es para que se asigne el ticket solo al usuario del token
     def perform_create(self, serializer):
         serializer.save(usuario=self.request.user)
+
