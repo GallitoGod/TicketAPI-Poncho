5758f875c39a9b5c86250fb18881f21d591cc00a
lautaro arce <lautaroarce345@gmail.com>
2026-05-29 18:03:46 -0300

    UPD 15 - Swagger

--- apps/core/views.py
+++ apps/core/views.py
@@ -4,7 +4,17 @@ from apps.core.serializer import EventoSerializer, SectorEntradaSerializer, Tick
 from apps.core.permissions import EsProductorOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from .filters import EventoFilter, SectorEntradaFilter, TicketFilter
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
+
+@extend_schema_view(
+    list=extend_schema(summary="Listar Eventos", description="Obtiene todos los eventos con los filtros y la paginación ya aplicada.", tags=['Eventos']),
+    create=extend_schema(summary="Crear Evento", description="Registra un nuevo evento", tags=['Eventos']),
+    retrieve=extend_schema(summary="Ver detalle de Evento", tags=['Eventos']),
+    update=extend_schema(summary="Actualizar Evento", description="Reemplaza completamente un evento.", tags=['Eventos']),
+    partial_update=extend_schema(summary="Actualizar parcialmente Evento", description="Modifica campos específicos del evento.", tags=['Eventos']),
+    destroy=extend_schema(summary="Eliminar Evento", description="Elimina un evento por UUID.", tags=['Eventos'])
+)
 class EventoViewSet(viewsets.ModelViewSet):
     queryset = Evento.objects.all()
     serializer_class = EventoSerializer
@@ -15,7 +25,14 @@ class EventoViewSet(viewsets.ModelViewSet):
     ordering_fields = ['fecha', 'nombre']
     filterset_class = EventoFilter
 
-
+@extend_schema_view(
+    list=extend_schema(summary="Listar Sectores", description="Obtiene todos los sectores de los eventos.", tags=['Sectores']),
+    create=extend_schema(summary="Crear Sector", description="Registra un nuevo Sector", tags=['Sectores']),
+    retrieve=extend_schema(summary="Ver detalle del Sector", tags=['Sectores']),
+    update=extend_schema(summary="Actualizar Sector", description="Reemplaza completamente un sector.", tags=['Sectores']),
+    partial_update=extend_schema(summary="Actualizar parcialmente Sector", description="Modifica campos específicos del sector.", tags=['Sectores']),
+    destroy=extend_schema(summary="Eliminar Sector", description="Elimina un sector por UUID.", tags=['Sectores'])
+)
 class SectorEntradaViewSet(viewsets.ModelViewSet):
     queryset = SectorEntrada.objects.all()
     serializer_class = SectorEntradaSerializer
@@ -25,6 +42,15 @@ class SectorEntradaViewSet(viewsets.ModelViewSet):
     ordering_fields = ['precio_base_ars']
     filterset_class = SectorEntradaFilter
 
+
+@extend_schema_view(
+    list=extend_schema(summary="Listar Tickets", description="Obtiene todos los tickets.", tags=['Tickets']),
+    create=extend_schema(summary="Crear Ticket", description="Registra un nuevo Ticket", tags=['Tickets']),
+    retrieve=extend_schema(summary="Ver detalle del Ticket", tags=['Tickets']),
+    update=extend_schema(summary="Actualizar Ticket", description="Reemplaza completamente un ticket.", tags=['Tickets']),
+    partial_update=extend_schema(summary="Actualizar parcialmente Ticket", description="Modifica campos específicos del ticket.", tags=['Tickets']),
+    destroy=extend_schema(summary="Eliminar Ticket", description="Elimina un ticket por UUID.", tags=['Tickets'])
+)
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,7 +1,16 @@
 from rest_framework import viewsets, permissions
 from .models import Usuario
 from apps.usuario.serializer import UsuarioSerializer
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
+@extend_schema_view(
+    list=extend_schema(summary="Listar Usuario", description="Obtiene todos los Usuarios.", tags=['Usuarios']),
+    create=extend_schema(summary="Crear Usuario", description="Registra un nuevo Usuario.", tags=['Usuarios']),
+    retrieve=extend_schema(summary="Ver detalle del Usuario", tags=['Usuarios']),
+    update=extend_schema(summary="Actualizar Usuario", description="Reemplaza completamente un Usuario.", tags=['Usuarios']),
+    partial_update=extend_schema(summary="Actualizar parcialmente Usuario", description="Modifica campos específicos del Usuario.", tags=['Usuarios']),
+    destroy=extend_schema(summary="Eliminar Usuario", description="Elimina un Usuario por UUID.", tags=['Usuarios'])
+)
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
